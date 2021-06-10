01a6d0c2f3a49ecebc88163f084f5a6cb859ca9c
NasarKamish <Nasarkamish@gmail.com>
2021-06-10 14:14:20 +0200

    Updated file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,22 @@ function toggleNavbar() {
   document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
 }
 
+let nav_anchors = document.querySelectorAll(".navbar-anchor");
+
+for (let i = 0; i < nav_anchors.length; i++) {
+  nav_anchors[i].addEventListener("click", toggleNavbar);
+}
+
 const Theme = document.querySelector(".Switch");
+
+let icon = document.querySelector(".fa-moon");
 
 Theme.addEventListener("click", function () {
   document.body.classList.toggle("Dark-Theme");
+
+  if (icon.className === "far fa-moon") {
+    icon.className = "far fa-sun";
+  } else if (icon.className === "far fa-sun") {
+    icon.className = "far fa-moon";
+  }
 });
