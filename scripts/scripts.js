30ad5ff25cda21e4b3f73a00cb43181def3cfb9a
NasarKamish <Nasarkamish@gmail.com>
2021-06-22 11:28:30 +0200

    Updated file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// Navbar
 function toggleNavbar() {
   document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
 }
@@ -7,6 +8,8 @@ let nav_anchors = document.querySelectorAll(".navbar-anchor");
 for (let i = 0; i < nav_anchors.length; i++) {
   nav_anchors[i].addEventListener("click", toggleNavbar);
 }
+
+// Theme changer
 
 const Theme = document.querySelector(".Switch");
 
@@ -23,3 +26,41 @@ Theme.addEventListener("click", function () {
     }
   }, 300);
 });
+
+// Filter buttons
+
+function borderclick(button) {
+  if (button == "all") {
+    let BTN = document.querySelector(".filter-all");
+    BTN.style.borderBottom = "solid";
+    BTN.style.borderTop = "solid";
+    BTN.style.borderColor = "var(--text-detail)";
+
+    BTN = document.querySelector(".filter-python");
+    BTN.style.border = "none";
+    BTN = document.querySelector(".filter-html");
+    BTN.style.border = "none";
+  }
+  if (button == "python") {
+    let BTN = document.querySelector(".filter-python");
+    BTN.style.borderBottom = "Solid";
+    BTN.style.borderTop = "Solid";
+    BTN.style.borderColor = "var(--text-detail)";
+
+    BTN = document.querySelector(".filter-all");
+    BTN.style.border = "none";
+    BTN = document.querySelector(".filter-html");
+    BTN.style.border = "none";
+  }
+  if (button == "html") {
+    let BTN = document.querySelector(".filter-html");
+    BTN.style.borderBottom = "Solid";
+    BTN.style.borderTop = "Solid";
+    BTN.style.borderColor = "var(--text-detail)";
+
+    BTN = document.querySelector(".filter-all");
+    BTN.style.border = "none";
+    BTN = document.querySelector(".filter-python");
+    BTN.style.border = "none";
+  }
+}
